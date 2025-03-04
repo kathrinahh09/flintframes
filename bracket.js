c0e74effb3bdb7eac6f59b5de6be2ccc7ae00c41
kathrinahh09 <kathrinahh09@gmail.com>
2025-03-04 14:18:12 +0800

    Update bracket.js

--- bracket.js
+++ bracket.js
@@ -21,14 +21,16 @@ async function generateBracket() {
     for (let i = 0; i < players.length; i += 2) {
         let match = {
             player1: players[i],
-            player2: players[i + 1] || { username: "Bye" },
+            player2: players[i + 1] || { username: "Bye", nfts: [] },
+            selectedCards: { player1: [], player2: [] },
             winner: null
         };
         matchups.push(match);
     }
 
-    matchups.forEach((match, index) => {
-        let winner = determineWinner(match.player1, match.player2);
+    for (let match of matchups) {
+        await selectBattleCards(match);
+        let winner = determineWinner(match);
         match.winner = winner;
 
         let matchElement = document.createElement("div");
@@ -38,18 +40,50 @@ async function generateBracket() {
             <strong>Winner: ${winner.username}</strong>
         `;
         bracket.appendChild(matchElement);
-    });
+    }
 
     return matchups;
 }
 
-function determineWinner(player1, player2) {
-    if (player2.username === "Bye") return player1;
+async function selectBattleCards(match) {
+    if (match.player2.username === "Bye") {
+        match.selectedCards.player1 = match.player1.nfts.slice(0, 3);
+        return;
+    }
 
-    let player1Score = player1.nfts.reduce((sum, nft) => sum + nft.attributes.power, 0);
-    let player2Score = player2.nfts.reduce((sum, nft) => sum + nft.attributes.power, 0);
+    return new Promise((resolve) => {
+        let countdown = 15;
+        let countdownElement = document.createElement("div");
+        countdownElement.classList.add("countdown");
+        document.body.appendChild(countdownElement);
 
-    return player1Score > player2Score ? player1 : player2;
+        let interval = setInterval(() => {
+            countdownElement.innerText = `Time remaining: ${countdown} seconds`;
+            countdown--;
+            if (countdown < 0) {
+                clearInterval(interval);
+                document.body.removeChild(countdownElement);
+                console.log("Time's up! Auto-selecting cards...");
+                match.selectedCards.player1 = getRandomCards(match.player1.nfts);
+                match.selectedCards.player2 = getRandomCards(match.player2.nfts);
+                resolve();
+            }
+        }, 1000);
+    });
+}
+
+function getRandomCards(nfts) {
+    let shuffled = nfts.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 3);
+}
+
+function determineWinner(match) {
+    if (match.player2.username === "Bye") return match.player1;
+
+    let player1Score = match.selectedCards.player1.reduce((sum, nft) => sum + nft.attributes.power, 0);
+    let player2Score = match.selectedCards.player2.reduce((sum, nft) => sum + nft.attributes.power, 0);
+
+    return player1Score > player2Score ? match.player1 : match.player2;
 }
 
 async function submitResults() {
